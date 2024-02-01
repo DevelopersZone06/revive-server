a303052467c6c1fc170104448473ad4e5b237bde
ibrahim-khalil0 <mdibrahimkhalil0183@gmail.com>
2024-02-01 11:48:31 +0600

    services, trainers, blogs events, api desing complete

--- index.js
+++ index.js
@@ -40,15 +40,90 @@ run().catch(console.dir);
 
 
 
-
-
-
+// all collection is here --------------
 
 const totalServices = client.db('revive').collection('services')
 const totalBlog = client.db('revive').collection('blogs')
+const totalTrainers= client.db('revive').collection('trainers')
+const totalUser = client.db('revive').collection('users')
+const totalNotification = client.db('revive').collection('notification')
+const totalEvents = client.db('revive').collection('events')
 
 
-// get all services 
+
+// all crud operation is here ---------------------------
+
+
+
+
+// all users crud operation is here ------
+
+// new user post api 
+app.post('/users', async(req, res) => {
+  const user = req.body
+  const result = await totalUser.insertOne(user)
+  res.send(result)
+})
+
+
+// get all user api 
+app.get('/users', async(req, res) => {
+  const users = await totalUser.find().toArray()
+  res.send(users)
+})
+
+
+
+
+// all trainers crud operation is here -------------------
+
+// get all trainers 
+app.get('/trainers', async(req, res) => {
+  const trainers = await totalTrainers.find().toArray()
+  res.send(trainers)
+})
+
+
+
+
+
+// all events crud operation is here ---------------
+
+// get all events 
+app.get('/events', async(req, res) => {
+  const events = await totalEvents.find().toArray()
+  res.send(events)
+})
+
+
+
+// new notification post operation 
+
+app.patch('/notification/:email', async (req, res) => {
+  const email = req.params.email;
+  const notification = req.body.notificationIs
+
+  const result = await totalNotification.updateOne(
+    { email: email },
+    {
+      $setOnInsert: { email: email }, 
+      $push: {
+        allNotification: {
+          $each: [notification],
+          $position: 0 
+        }
+      } 
+    },
+    { upsert: true } 
+  );
+  res.send(result)
+})
+
+
+
+
+
+// all services api is here
 
 app.get('/services', async (req, res) => {
     const services = await totalServices.find().toArray()
